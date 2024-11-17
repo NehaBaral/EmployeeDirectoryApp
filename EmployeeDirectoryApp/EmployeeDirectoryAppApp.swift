baa76bced1fc7eccfb5d7912dac990b657048469
NehaBaral <41846320+NehaBaral@users.noreply.github.com>
2024-11-16 22:23:19 -0500

    Initial Commit

---
+++ EmployeeDirectoryApp/EmployeeDirectoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeDirectoryAppApp.swift
+//  EmployeeDirectoryApp
+//
+//  Created by Neha on 2024-11-16.
+//
+
+import SwiftUI
+
+@main
+struct EmployeeDirectoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
