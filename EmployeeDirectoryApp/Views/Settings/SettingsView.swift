5533a80dcbd9f161a0f080db2b5c661fc9869326
NehaBaral <41846320+NehaBaral@users.noreply.github.com>
2024-11-18 23:40:57 -0500

    Settings screen updated

--- EmployeeDirectoryApp/Views/Settings/SettingsView.swift
+++ EmployeeDirectoryApp/Views/Settings/SettingsView.swift
@@ -9,9 +9,32 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            NavigationView {
+                Form {
+                    Section(header: Text("App Information")) {
+                        VStack(alignment: .leading) {
+                            Text("App Name: Employee Directory")
+                                .font(.headline)
+                            Text("It displays a list of employees fetched from a JSON endpoint, search functionality for easy access to employee details. It features an onboarding screen for first-time users. Images are cached locally to optimize performance. It also includes a settings screen that provide information about the app and its developer.")
+                                .font(.subheadline)
+                        }
+                    }
+                    
+                    Section(header: Text("Student Information")) {
+                        VStack(alignment: .leading) {
+                            Text("Name: Neha Baral")
+                                .font(.headline)
+                            Text("Student Number: 1265656")
+                                .font(.subheadline)
+                        }
+                    }
+                }
+                .navigationTitle("Settings")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .background(Color(red: 0.95, green: 0.95, blue: 0.95)) // Light gray background
+        }
     }
-}
 
 #Preview {
     SettingsView()
