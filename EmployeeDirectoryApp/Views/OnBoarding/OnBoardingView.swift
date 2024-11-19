f3a6a704d5263f20a0742c65d050090add7b9347
NehaBaral <41846320+NehaBaral@users.noreply.github.com>
2024-11-18 23:28:00 -0500

    OnBoarding screen UI updated

--- EmployeeDirectoryApp/Views/OnBoarding/OnBoardingView.swift
+++ EmployeeDirectoryApp/Views/OnBoarding/OnBoardingView.swift
@@ -12,11 +12,41 @@ struct OnBoardingView: View {
     
     var body: some View {
         VStack{
-            Text("Hello World")
+            Text("Welcome to Employee List")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+                .padding(.vertical, 16)
+                .padding(.top, 8)
+            
             Spacer()
-            Button("Next"){
+                .frame(height: 80)
+            
+            Image("onboarding_logo")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200, height: 200)
+            
+            Text("Manage your employees efficiently and stay updated with real-time information.")
+            .font(.body)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal)
+            .foregroundColor(.white)
+            
+            Spacer()
+            Button(action: {
                 onBoardingRequired = false
-            }
+            }) {
+                Text("Get Started")
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .frame(width: 200, height: 50)
+                .background(Color(red: 223/255, green: 43/255, blue: 12/255))
+                .cornerRadius(10)
+            }.padding(.bottom, 50)
         }
+        .padding()
+        .background(Color(red: 12/255, green: 192/255, blue: 223/255))
+        .edgesIgnoringSafeArea(.all)
     }
 }
